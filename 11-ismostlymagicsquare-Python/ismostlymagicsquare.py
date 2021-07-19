8060d52acff9ac6f7d2ae9820e40b1c675dced98
RohitValluri <rohitntr999@msitprogram.net>
2021-07-19 20:24:47 +0530

    first commit

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -14,5 +14,25 @@
 # Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
 def ismostlymagicsquare(a):
-	# Your code goes here
-	pass
+    firstdiagonalcount=0
+    list1=[]
+    list2=[]
+    seconddiagonalcount=0
+    row=len(a)
+    col=len(a[0])
+    if row==col:
+        for i in range(0,row):
+            firstdiagonalcount+=a[i][i]
+            seconddiagonalcount+=a[i][row-i-1]
+            rowsum=0
+            colcount=0
+            for j in range(0,col):
+                rowsum+=a[i][j]                
+                colcount+=a[j][i]
+            list2.append(colcount)
+            list1.append(rowsum)
+            list2=list(set(list2))
+            list1=list(set(list1))
+            if len(list2)==len(list1)==1 and list2[0]==list1[0]==firstdiagonalcount==seconddiagonalcount:
+                return True
+    return False
