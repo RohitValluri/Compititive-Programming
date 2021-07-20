6181f50941252fd4020979eb849f3b0492e60a6b
RohitValluri <rohitntr999@msitprogram.net>
2021-07-20 12:13:43 +0530

    first commit

--- 04-stringkeys-Python/stringkeys.py
+++ 04-stringkeys-Python/stringkeys.py
@@ -1,11 +1,6 @@
 """Write a HashTable class that stores strings
 in a hash table, where keys are calculated
 using the first two letters of the string."""
-class Node:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
 
 class HashTable(object):
     def __init__(self):
@@ -17,19 +12,24 @@ class HashTable(object):
         the table."""
         # Hash Value = (ASCII Value of First Letter * 100) + ASCII Value of Second Letter 
         # Your code goes here
-        pass
+        hashvalue = self.calculate_hash_value(string)
+        self.table[hashvalue] = string
         
     def lookup(self, string):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
         # Your code goes here
-        pass
+        if string in self.table :
+            return self.calculate_hash_value(string)
+        else:
+            return -1
         
     def calculate_hash_value(self, string):
         """Helper function to calulate a
         hash value from a string."""
         # Your code goes here
-        pass
+        hashvalue = (ord(string[0])*100) + ord(string[1])
+        return hashvalue
 
 
