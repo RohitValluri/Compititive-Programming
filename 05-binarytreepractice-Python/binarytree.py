eb0ae9e601649af863a2d39d8c9043050706914c
RohitValluri <rohitntr999@msitprogram.net>
2021-07-26 20:06:40 +0530

    first commit

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -1,3 +1,6 @@
+# from _typeshed import Self
+
+
 class Node(object):
     def __init__(self, value):
         self.value = value
@@ -12,25 +15,36 @@ class BinaryTree(object):
         """
         Return True if the find_val is in the tree and False otherwise.
         """
-        # Your code goes here
-        pass
+        
+        
+        
 
     def print_tree(self):
         """
         Print out all tree nodes as they are visited in a pre-order traversal."""
         # Your code goes here
-        pass
+        self = Node(self)
+        if self.left:
+            self.left.print_tree()
+        print(self.value),
+        if self.right:
+            self.right.print_tree()
+        # pass
 
     def preorder_search(self, start, find_val):
         """
         Helper method - use this to create a recursive search solution.
         """
         # Your code goes here
-        pass
+        # pass
 
     def preorder_print(self, start, traversal):
         """
         Helper method - use this to create a recursive print solution.
         """
         # Your code goes here
+        
         pass
+root = BinaryTree(15)
+# print(root.search(4))
+print(root.print_tree())
