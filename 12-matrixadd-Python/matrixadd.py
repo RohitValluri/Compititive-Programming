59fe3b7366af61514a4b72c1058235e0106e5205
RohitValluri <rohitntr999@msitprogram.net>
2021-07-27 12:05:01 +0530

    first commit

--- 12-matrixadd-Python/matrixadd.py
+++ 12-matrixadd-Python/matrixadd.py
@@ -17,4 +17,19 @@
 
 def matrixadd(L, M):
 	# Your code goes here
-	pass
+	# N = [len(L)][len(L[0])]
+	# if (len(L)==len(M) and len(L[0])==len(M[0])):
+	# 	for i in range(len(L)):
+	# 		for j in range(len(L[0])):
+	# 			N = L[i][j] + M[i][j]
+	# 	for x in range(len(N)):
+	# 		return x
+	# else:
+	# 	return None	
+		result = [map(sum, zip(*i)) for i in zip(L, M)]
+		 
+		return result
+
+
+print(matrixadd([[1,  2, 3],[4,  5,  6]], [[21, 22, 23], [24, 25, 26]]))
+# print(matrixadd([[1,  2,  3],[4,  5,  6]], [[21, 22, 23], [24, 25]]))	
