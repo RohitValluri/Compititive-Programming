e9fb8f2a53bb93fc031c0b54ab50ec45042d34bd
RohitValluri <rohitntr999@msitprogram.net>
2021-08-05 10:55:07 +0530

    first commit

--- quiz.py
+++ quiz.py
@@ -1,13 +1,50 @@
-# Write the function bestQuiz(a), which takes a rectangular 2d list of numbers that represents a gradebook, where each column represents a quiz, and each row represents a student, and each value represents that student's score on that quiz (except -1 indicates the student did not take the quiz). For example:
+# Write the function bestQuiz(a), which takes a rectangular 2d list of numbers that represents a gradebook, 
+# where each column represents a quiz, and each row represents a student, and each value represents 
+# that student's score on that quiz (except -1 indicates the student did not take the quiz). For example:
 #   a = [ [ 88,  80, 91 ],
 #         [ 68, 100, -1 ]
 #       ]
-# This list indicates that student0 scored 88 on quiz0, 80 on quiz1, and 91 on quiz2. Also, student1 scored 68 on quiz0, 100 on quiz1, and did not take quiz2. The function returns the quiz with the highest average. In this case, quiz0 average is 78, quiz1 average is 90, and quiz2 average is 91 (since we ignore the -1). Thus, quiz2 is the best, and so the function returns 2 in this case. You are not responsible for malformed input, except you should return None if there are no quizzes. Also, resolve ties in favor of the lower quiz number. Here is a test function for you:
+# This list indicates that student0 scored 88 on quiz0, 80 on quiz1, and 91 on quiz2. 
+# Also, student1 scored 68 on quiz0, 100 on quiz1, and did not take quiz2. 
+# The function returns the quiz with the highest average. 
+# In this case, quiz0 average is 78, quiz1 average is 90, and quiz2 average is 91 (since we ignore the -1). 
+# Thus, quiz2 is the best, and so the function returns 2 in this case. 
+# You are not responsible for malformed input, except you should return None if there are no quizzes. 
+# Also, resolve ties in favor of the lower quiz number. Here is a test function for you:
 
 def bestQuiz(l):
-    # Your  code goes ehre...
-    pass
 
+    # Your  code goes ehre...
+      y =[]
+      li = list(set(l[0]))
+      li1 = list(set(l[1]))
+      if(li==li1):
+            return None
+      else:
+            for i in range(len(l)):
+                  for j in range(len(l[0])):
+                        x=0
+                        if(l[i][j]==-1):
+                              x = l[i+1][j]
+                        elif(l[i+1][j]==-1):
+                              x = l[i][j]
+                        else:      
+                              x = (l[i][j]+l[i+1][j])//2
+                        y.append(x)
+                  break
+            # print("y:",y)
+            maximum = max(y)
+            # print("result:",y.index(maximum))
+            return  y.index(maximum)            
+      # pass
+# print(bestQuiz([ [ 88,  80, 91 ],
+#           [ 68, 100, -1 ]]))
+# print(bestQuiz([ [ 88,  80, 80 ],
+#           [ 68, 100, 100 ]]))
+# print(bestQuiz([ [-1, -1, -1 ],
+#           [-1, -1, -1 ]]))
+# print(bestQuiz([ [88, -1, -1 ],
+#           [68, -1, -1 ]]))                        
 def testBestQuiz():
     print('Testing bestQuiz()...', end='')
     a = [ [ 88,  80, 91 ],
@@ -23,3 +60,5 @@ def testBestQuiz():
           [-1, -1, -1 ]]
     assert(bestQuiz(a) == None)
     print('All test cases passed...!')
+
+print(testBestQuiz())    
