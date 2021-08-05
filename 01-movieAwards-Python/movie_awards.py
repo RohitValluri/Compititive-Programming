5cf7a53e59e95145854c669b221c1ada4b4298a5
RohitValluri <rohitntr999@msitprogram.net>
2021-08-05 12:18:43 +0530

    first commit

--- 01-movieAwards-Python/movie_awards.py
+++ 01-movieAwards-Python/movie_awards.py
@@ -1,6 +1,8 @@
 '''
 movieAwards(oscarResults) [10 pts]
-Write the function movieAwards(oscarResults) that takes a set of tuples, where each tuple holds the name of a category and the name of the winning movie, then returns a dictionary mapping each movie to the number of the awards that it won. For example, if we provide the set:
+Write the function movieAwards(oscarResults) that takes a set of tuples, where each tuple holds 
+the name of a category and the name of the winning movie, then returns a dictionary mapping 
+each movie to the number of the awards that it won. For example, if we provide the set:
 { 
     ("Best Picture", "The Shape of Water"), 
     ("Best Actor", "Darkest Hour"),
@@ -18,7 +20,29 @@ the function should return as follows
     "I, Tonya" : 1
 }
 '''
-
+from collections import Counter
 def movie_awards(oscarResults):
     # Your code goes here...
-    pass
+    l=[]
+    for i in oscarResults:
+        i=list(i)
+        l.append(i)
+    li=[]
+    count=0
+    for j in l:
+        if j[1] not in li:
+            count+=1   
+        li.append(j[1])
+        # print("li:",li)
+    x = Counter(li)
+    return x
+    # pass
+# print(movie_awards({ 
+#     ("Best Picture", "The Shape of Water"), 
+#     ("Best Actor", "Darkest Hour"),
+#     ("Best Actress", "Three Billboards Outside Ebbing, Missouri"),
+#     ("Best Director", "The Shape of Water"),
+#     ("Best Supporting Actor", "Three Billboards Outside Ebbing, Missouri"),
+#     ("Best Supporting Actress", "I, Tonya"),
+#     ("Best Original Score", "The Shape of Water")
+# }))
